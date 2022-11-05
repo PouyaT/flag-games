307b4200f10c6921f09ab44236ac1a62e910715f
Pouya Tavakoli <1pouya.tavakoli@gmail.com>
2022-11-05 15:46:14 -0500

    fix: made sure the secret key was being read a string

--- flag_games/flag_games/settings.py
+++ flag_games/flag_games/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = my_secret_key
+SECRET_KEY = str(my_secret_key)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
