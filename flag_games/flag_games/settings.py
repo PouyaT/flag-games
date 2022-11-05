43231091c5a6447953e6809d658c0729dabc2369
Pouya Tavakoli <1pouya.tavakoli@gmail.com>
2022-11-05 17:09:42 -0500

    fix: adding staticfiles in setting.py#

--- flag_games/flag_games/settings.py
+++ flag_games/flag_games/settings.py
@@ -118,12 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
