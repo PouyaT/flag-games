44d7e394a7e46700d8b588beb4366737d457556c
Pouya Tavakoli <1pouya.tavakoli@gmail.com>
2022-11-05 16:03:05 -0500

    fix: Adding static file bc I think I need it for pythonanywhere

--- flag_games/flag_games/settings.py
+++ flag_games/flag_games/settings.py
@@ -121,6 +121,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+STATIC_ROOT = "/home/PTavakoli/flag-games/static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
