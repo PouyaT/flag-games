11c85d5d79381a1f006e1903607c7a1216f03cd6
ptavakoli <1pouya.tavakoli@gmail.com>
2023-04-20 21:34:03 -0500

    feat: added staticfiles_storage since chatgpt informed that I need that set for it to actually work

--- flag_games/flag_games/settings.py
+++ flag_games/flag_games/settings.py
@@ -56,6 +56,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 ROOT_URLCONF = 'flag_games.urls'
 
 TEMPLATES = [
