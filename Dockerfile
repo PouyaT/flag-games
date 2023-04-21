61c7cb4d712dae75abdc27fb4d836bcbbb324669
ptavakoli <1pouya.tavakoli@gmail.com>
2023-04-21 00:14:55 -0500

    fix: properly setup whitenoise so django is now prod read and removed static files folder since the dockerfile will now run that command when built

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,11 @@ WORKDIR $DockerHOME
 COPY . $DockerHOME
 # install dependencies
 RUN pip install -r requirements.txt
-
+# move working dir to where manage.py is
 WORKDIR $DockerHOME/flag_games
+# set default command (I thinkk)
+ENTRYPOINT ["python"]
+# run commands for app to run
+CMD ["manage.py", "collectstatic", "--noinput"]
+CMD ["manage.py", "runserver", "0.0.0.0:8000"]
 
--- flag_games/flag_games/settings.py
+++ flag_games/flag_games/settings.py
@@ -40,12 +40,14 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    "whitenoise.runserver_nostatic",
     'django.contrib.staticfiles',
 ]
 
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -54,6 +56,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 ROOT_URLCONF = 'flag_games.urls'
 
 TEMPLATES = [
