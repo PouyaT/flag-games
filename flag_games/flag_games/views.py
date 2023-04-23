aac9ae7e0f437acbfb70969ad3ba3a5ed32351c5
ptavakoli <1pouya.tavakoli@gmail.com>
2023-04-22 23:16:52 -0500

    feat: added a health check endpoint

--- flag_games/flag_games/views.py
+++ flag_games/flag_games/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
 
 def index(request):
     return render(request, 'flag_games/index.html')
+
+
+def health_check(request):
+    # Perform any checks to determine the health of your application
+    is_healthy = True
+
+    # Return a JSON response with the health status
+    if is_healthy:
+        return JsonResponse({'status': 'ok'})
+    else:
+        return JsonResponse({'status': 'error'}, status=503)
